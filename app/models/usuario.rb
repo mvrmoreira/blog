d6a5553e8619ae3ee663e4eb3ad11a93793a31d9
Matheus Moreira <mvrmoreira@gmail.com>
2012-09-11 19:23:09 -0300

    correcao validacao usuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,5 +1,5 @@
 class Usuario < ActiveRecord::Base
   validates :nome, :presence => true
-  validates :email, :uniqueness => true, :format => :email, :with => /regex/i
+  validates :email, :uniqueness => true
   validates :cpf, :numericality => true, :length => {:is => 11}
 end
