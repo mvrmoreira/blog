7d122172445b5952989a0a816465e5e5128df770
Matheus Moreira <mvrmoreira@gmail.com>
2012-09-11 19:19:43 -0300

    adicao validacao usuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,2 +1,5 @@
 class Usuario < ActiveRecord::Base
+  validates :nome, :presence => true
+  validates :email, :uniqueness => true, :format => :email, :with => /regex/i
+  validates :cpf, :numericality => true, :length => {:is => 11}
 end
